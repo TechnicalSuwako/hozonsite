f45290221e4b9120858d86c33a05c8d35a93eecc
諏訪子 <suwako@076.moe>
2024-04-10 15:12:59 +0900

    変なエンコーディングの変換

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module gitler.moe/suwako/hozonsite
 go 1.20
 
 require gitler.moe/suwako/goliblocale v1.0.0
+
+require golang.org/x/text v0.14.0 // indirect
--- src/getpage.go
+++ src/getpage.go
@@ -5,7 +5,11 @@ import (
   "fmt"
   "net/http"
   "io"
+  "regexp"
   "strings"
+
+  "golang.org/x/text/encoding/japanese"
+  "golang.org/x/text/transform"
 )
 
 // URLでパラメートル（?、=等）がある場合
@@ -22,7 +26,8 @@ func Getpage (url string, path string) {
     fmt.Println("CURLエラー：", err)
     return
   }
-  defer curl.Body.Close() // ソフトの終了する時に実行する
+  // ソフトの終了する時に実行する
+  defer curl.Body.Close()
 
   // ページの内容を読み込む
   body, err2 := io.ReadAll(curl.Body)
@@ -31,17 +36,62 @@ func Getpage (url string, path string) {
     return
   }
 
-  // 空index.htmlファイルを創作する
-  fn, err3 := os.Create(path + "/index.html")
-  if err3 != nil {
-    fmt.Println("ファイルの創作エラー：", err3)
+  // Content-TypeヘッダーはUTF-8又は駄目のエンコーディングかの確認
+  checkJis := `(?i)<meta.*?charset=(["']?)shift[_-]?jis`
+  jisRegex, errr := regexp.Compile(checkJis)
+  if errr != nil {
+    fmt.Println(errr)
     return
   }
-  defer fn.Close() // ソフトの終了する時に実行する
+
+  checkEuc := `(?i)<meta.*?charset=(["']?)euc[_-]?jp`
+  eucRegex, erre := regexp.Compile(checkEuc)
+  if erre != nil {
+    fmt.Println(erre)
+    return
+  }
+
+  // 文字エンコーディングを変換する
+  if jisRegex.Match(body) {
+    shiftJISDecoder := japanese.ShiftJIS.NewDecoder()
+    utf8Reader := transform.NewReader(
+      strings.NewReader(string(body)),
+      shiftJISDecoder,
+    )
+    utf8Body, err3 := io.ReadAll(utf8Reader)
+    if err3 != nil {
+      fmt.Println("文字エンコーディング変換エラー：", err3)
+      return
+    }
+
+    body = utf8Body
+  } else if eucRegex.Match(body) {
+    eucJPDecoder := japanese.EUCJP.NewDecoder()
+    utf8Reader := transform.NewReader(
+      strings.NewReader(string(body)),
+      eucJPDecoder,
+    )
+    utf8Body, err3 := io.ReadAll(utf8Reader)
+    if err3 != nil {
+      fmt.Println("文字エンコーディング変換エラー：", err3)
+      return
+    }
+
+    body = utf8Body
+  }
+
+  // 空index.htmlファイルを創作する
+  fn, err4 := os.Create(path + "/index.html")
+  if err4 != nil {
+    fmt.Println("ファイルの創作エラー：", err4)
+    return
+  }
+  // ソフトの終了する時に実行する
+  defer fn.Close()
 
   // あのindex.htmlファイルに内容をそのまま書き込む
-  _, err4 := fn.WriteString(string(body))
-  if err4 != nil {
-    fmt.Println("ファイル書込エラー：", err4)
+  _, err5 := fn.WriteString(string(body))
+  if err5 != nil {
+    fmt.Println("ファイル書込エラー：", err5)
   }
 }
